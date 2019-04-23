40dea05829d47b1a0b8bc29e8e5d036b376ddc77
Stenli <stanil_dimitrov@mail.bg>
2019-04-23 15:08:22 +0300

    Genre view model.

--- MoviesCatalog/MoviesCatalog.Web/Models/ActorViewModel.cs
+++ MoviesCatalog/MoviesCatalog.Web/Models/ActorViewModel.cs
@@ -1,6 +1,22 @@
-﻿namespace MoviesCatalog.Web.Models
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MoviesCatalog.Web.Models
 {
     public class ActorViewModel
     {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        public string Biography { get; set; }
+
+        public ICollection<MovieViewModel> Movies { get; set; }
     }
 }
--- MoviesCatalog/MoviesCatalog.Web/Models/ReviewViewModel.cs
+++ MoviesCatalog/MoviesCatalog.Web/Models/ReviewViewModel.cs
@@ -1,6 +1,26 @@
-﻿namespace MoviesCatalog.Web.Models
+﻿using MoviesCatalog.Data.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MoviesCatalog.Web.Models
 {
     public class ReviewViewModel
     {
+        public int Id { get; set; }
+
+        [MaxLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        public int Rating { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        public int MovieId { get; set; }
+        public Movie Movie { get; set; }
     }
 }
