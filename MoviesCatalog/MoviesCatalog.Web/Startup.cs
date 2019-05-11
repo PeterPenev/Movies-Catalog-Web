9d38da57cb995de5b335b95494406d8b12bc2712
Stenli <stanil_dimitrov@mail.bg>
2019-05-11 20:36:58 +0300

    version 1.01

--- MoviesCatalog/MoviesCatalog.Web/Startup.cs
+++ MoviesCatalog/MoviesCatalog.Web/Startup.cs
@@ -60,7 +60,7 @@ namespace MoviesCatalog.Web
             }
             else
             {
-                app.UseExceptionHandler("Oops, something went wrong");
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
