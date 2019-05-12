d47d82884b7b1570c83e2ed5ec995fee44aeb806
Stenli <stanil_dimitrov@mail.bg>
2019-05-12 10:00:04 +0300

    Improvements

---
+++ MoviesCatalog/MoviesCatalog.Data/Migrations/20190512061458_Add-Migraion-Last.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoviesCatalog.Data.Migrations
+{
+    public partial class AddMigraionLast : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SliderImage",
+                table: "Movies");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Avatar",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Avatar",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SliderImage",
+                table: "Movies",
+                nullable: true);
+        }
+    }
+}
